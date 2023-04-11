76d0e67870d1c21d1b87be5e48975ee4eb1c1232
Gayatri Satish Suryawanshi <89183954+Gayatri3108@users.noreply.github.com>
2023-04-11 18:06:30 +0530

    first commit

---
+++ SpringWebAppUsingMVC/src/main/java/org/tnsif/springweb/SpringWebAppUsingMvcApplication.java
@@ -0,0 +1,15 @@
+package org.tnsif.springweb;
+
+import java.util.Scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebAppUsingMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebAppUsingMvcApplication.class, args);
+	}
+	}
+
